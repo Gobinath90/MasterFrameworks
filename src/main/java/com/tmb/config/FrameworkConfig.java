a4df0d29b51ca7205451903c77483e3ace9f3850
gobinath90 <ravi.gopi90@gmail.com>
2023-05-30 15:13:34 +0530

    Added Config File
    ConfigFactory
    FrameworkConfig

---
+++ src/main/java/com/tmb/config/FrameworkConfig.java
@@ -0,0 +1,25 @@
+package com.tmb.config;
+
+import com.tmb.config.converters.StringToBrowserTypeConverter;
+import com.tmb.enums.BrowserRemoteModeType;
+import com.tmb.enums.BrowserType;
+import com.tmb.enums.RunModeBrowserType;
+import org.aeonbits.owner.Config;
+@Config.LoadPolicy(Config.LoadType.MERGE)
+@Config.Sources({
+        "system:properties",
+        "system:env",
+        "file:${user.dir}/src/test/resources/config.properties"
+}
+
+)
+public interface FrameworkConfig extends Config {
+    @DefaultValue("CHROME")
+    @ConverterClass(StringToBrowserTypeConverter.class)
+
+    BrowserType browser();
+    @Key("runModeBrowser")
+    RunModeBrowserType browserRunMode();
+    @Key("browserRemoteMode")
+    BrowserRemoteModeType browserRemoteMode();
+}
